d44559b2577dd48963db5891275633da354a6fd6
Hayden Prairie <hprairie17@gmail.com>
2024-11-17 20:21:32 -0600

    rough implementation of runtime class

--- src/catapult/runtime/jit.py
+++ src/catapult/runtime/jit.py
@@ -1,4 +1,7 @@
-from catapult.compiler.compiler import create_program
+import ctypes
+import torch
+from cuda import cuda, driver
+from catapult.compiler.compiler import create_program, checkCudaErrors
 from typing import List, TypeVar, Generic, Optional, overload, Callable, Union
 
 T = TypeVar("T")
@@ -30,6 +33,10 @@ class KernelParams:
             source=kernel_path, name=kernel_name, num_headers=0, headers=None, include_names=None, method=self.method
         )
 
+    def __del__(self):
+        del self.program
+        return
+
     def get_compiled_kernel(self, options, named_expression):
         if options is not None:
             raise NotImplementedError("Compiling with options is not currently enabled.")
@@ -44,8 +51,8 @@ class KernelInterface(Generic[T]):
 
     run: T
 
-    def __getitem__(self, grid) -> T:
-        return lambda *args, **kwargs: self.run(grid=grid, warmup=False, *args, **kwargs)
+    def __getitem__(self, grid, thread_grid) -> T:
+        return lambda *args, **kwargs: self.run(grid=grid, thread_grid=thread_grid, warmup=False, *args, **kwargs)
 
 
 class JITKernel(KernelInterface[T]):
@@ -73,12 +80,105 @@ class JITKernel(KernelInterface[T]):
         self.compile_options = compile_options
         self.launch_metadata = launch_metadata
 
+        if not torch.cuda.is_initialized():
+            torch.cuda.init()
+
+        # Get compute capability and architecture argument
+        self.cuDevice = checkCudaErrors(cuda.cuDeviceGet(torch.cuda.current_device()))
+        self.major = checkCudaErrors(
+            cuda.cuDeviceGetAttribute(
+                cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, self.cuDevice
+            )
+        )
+        self.minor = checkCudaErrors(
+            cuda.cuDeviceGetAttribute(
+                cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, self.cuDevice
+            )
+        )
+
     @staticmethod
     def _clean_values(*args):
-        pass
+        """
+        Prepares arguments for CUDA kernel launch with proper C types using a dictionary-based approach.
 
-    def run(self, grid=None, warmup=None, *args, **kwargs):
-        pass
+        Args:
+            *args: Variable number of arguments of different types
+
+        Returns:
+            tuple: (arg_values, arg_types) for cuLaunchKernel
+        """
+
+        # Dictionary mapping Python types to their corresponding ctype handlers
+
+        TYPE_HANDLERS = {
+            torch.Tensor: lambda x: (x.data_ptr(), ctypes.c_void_p),
+            bool: lambda x: (ctypes.c_bool(x), ctypes.c_bool),
+            int: lambda x: (ctypes.c_size_t(x), ctypes.c_size_t),
+            float: lambda x: (ctypes.c_float(x), ctypes.c_float),
+        }
+
+        def get_ctype_and_value(arg):
+            # Look up the handler for this type
+            handler = TYPE_HANDLERS.get(type(arg))
+            if handler is None:
+                raise TypeError(f"Unsupported argument type: {type(arg)}")
+            return handler(arg)
+
+        # Process all arguments
+        processed_args = [get_ctype_and_value(arg) for arg in args]
+
+        # Split into values and types
+        arg_values = tuple(value for value, _ in processed_args)
+        arg_types = tuple(type_ for _, type_ in processed_args)
+
+        return arg_values, arg_types
+
+    @staticmethod
+    def _get_stream():
+        stream = torch.cuda.current_stream()
+        if stream.cuda_stream == 0:
+            new_stream = torch.cuda.Stream()
+            torch.cuda.set_stream(new_stream)
+        stream = torch.cuda.current_stream()
+        return stream
+
+    def __del__(self):
+        del self.kernel_params
+        return
+
+    def run(self, grid=None, thread_grid=None, warmup=None, *args, **kwargs):
+
+        if kwargs is not None:
+            raise NotImplementedError("Passing template values as kwargs is not supported currently")
+        # TODO: Make better errors
+        if grid is None:
+            raise ValueError("GRID IS NONE")
+        if thread_grid is None:
+            raise ValueError("THREAD GRID IS NONE")
+
+        # TODO add caching
+        kernel = self.kernel_params.get_compiled_kernel(options=None, named_expression=None)
+
+        stream = self._get_stream()
+
+        arg_values, arg_types = self._clean_values(args)
+
+        checkCudaErrors(
+            cuda.cuLaunchKernel(
+                kernel,
+                grid[0],
+                grid[1],
+                grid[2],
+                thread_grid[0],
+                thread_grid[1],
+                thread_grid[2],
+                0,
+                stream.cuda_stream,
+                (arg_values, arg_types),
+                0,
+            )
+        )
+        return
 
 
 # ---------------------------
