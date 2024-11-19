6471471c3ac9d4eebbb34ef58f109a2e117c84ae
Hayden Prairie <hprairie17@gmail.com>
2024-11-18 19:15:06 -0600

    added types for templating

--- src/catapult/runtime/__init__.py
+++ src/catapult/runtime/__init__.py
@@ -1,7 +1,37 @@
 from .jit import JITKernel, KernelInterface
+from .types import (
+    int1,
+    int8,
+    int16,
+    int32,
+    int64,
+    float16,
+    float32,
+    float64,
+    bfloat16,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+    void,
+)
 
 
 __all__ = [
     "JITKernel",
     "KernelInterface",
+    "int1",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "float16",
+    "float32",
+    "float64",
+    "bfloat16",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "void",
 ]
