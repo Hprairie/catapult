eca9eb9939569f5a6f235a17e4d688e5641e7e0a
frank collebrusco <collebrusco.frank@gmail.com>
2025-02-17 02:47:36 -0600

    use pytest properly in tb

--- testbench/test_simple.py
+++ testbench/test_simple.py
@@ -1,30 +1,26 @@
+import pytest
 import torch
 import catapult
 
-@catapult.jit(kernel_path='cu/simple.cu',kernel_name='global_reduce_kernel')
+@catapult.jit(kernel_path='cu/simple.cu', kernel_name='global_reduce_kernel')
 def greduce(kernel, blk, N, din):
     output = torch.zeros_like(din)
     kernel[(blk, 1, 1), (N, 1, 1)](output, din)
     return output
 
-
-def test_reduce(N):
+@pytest.fixture
+def cuda_device_verify():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if device.type != "cuda":
-        raise RuntimeError("CUDA device not available.")
-    
-    inp = torch.rand(N, device=device, dtype=torch.float32)
+        pytest.skip("Skipping test: CUDA device not available")
+    return device
+
+@pytest.mark.parametrize("N", [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024])
+def test_reduce(cuda_device, N):
+""" test a basic reduce kernel on various input sizes """
+    inp = torch.rand(N, device=cuda_device, dtype=torch.float32)
     exp = inp.sum()
 
     out = greduce(1, N, inp)[0]
-    print(f'g: expect {exp}, get {out}')
-    assert(torch.allclose(exp, out))
-    
 
-
-if __name__ == '__main__':
-    n = 1
-    while n <= 1024:
-        test_reduce(n)
-        n <<= 1
-    print('PASS')
+    assert torch.allclose(exp, out), f"Expected {exp}, but got {out}"
