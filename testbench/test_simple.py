81fc6f5474986c472643549a58924dac39cb85b7
frank collebrusco <collebrusco.frank@gmail.com>
2025-02-17 03:28:32 -0600

    add fft tests and some more setup

--- testbench/test_simple.py
+++ testbench/test_simple.py
@@ -8,16 +8,9 @@ def greduce(kernel, blk, N, din):
     kernel[(blk, 1, 1), (N, 1, 1)](output, din)
     return output
 
-@pytest.fixture
-def cuda_device_verify():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if device.type != "cuda":
-        pytest.skip("Skipping test: CUDA device not available")
-    return device
-
 @pytest.mark.parametrize("N", [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024])
 def test_reduce(cuda_device, N):
-""" test a basic reduce kernel on various input sizes """
+    """ test a basic reduce kernel on various input sizes """
     inp = torch.rand(N, device=cuda_device, dtype=torch.float32)
     exp = inp.sum()
 
